6af3a4296904cb7117e2a6a44420a350bf9b67de
irongeek <mriscott@yahoo,co,uk>
2022-09-30 13:52:39 +0100

    Stored whole program and implemented jmp

--- machine.c
+++ machine.c
@@ -2,20 +2,34 @@
 
 FILE * fp;
 
+#define MAX (30050)
+int program [MAX];
+int idx;
 
 //#define DEBUG printf
 #define DEBUG //
+
+
+int getnum(){
+  int num=program[idx];
+  idx++;
+  return num;
+}
+
 short readnum(){
 	short code=0;
-	fread(&code,sizeof(short),1,fp);
+	if(fread(&code,sizeof(short),1,fp)==0){
+	  return -1;
+	}
 	return code;
 }
 
 
 
 // return: 0 - ok, 1 - halt, 2 - error
-int runcmd(int x){
+int runcmd(){
 	int a,b,c;
+	int x=getnum();
 	DEBUG("Command:%d\n",x);
 	switch (x){
 	case 0:
@@ -23,6 +37,25 @@ int runcmd(int x){
 	  return 1;
 	  break;
 
+	case 6:
+	  //jmp: 6 a
+	  //jump to <a>
+	  idx=getnum();
+	  break;
+	
+	case 19:
+	  //out: 19 a
+	  //write the character represented by ascii code <a> to the terminal
+	  a=getnum();
+	  printf("%c",a);
+	  break;
+	
+	case 21:
+	  //noop
+	  printf("noop\n");
+	  break;
+
+	  // unimplemented codes here
 	case 1:
 	  //set: 1 a b
 	  //  set register <a> to the value of <b>
@@ -38,9 +71,6 @@ int runcmd(int x){
 	case 5:
 	  //gt: 5 a b c
 	  //set <a> to 1 if <b> is greater than <c>; set it to 0 otherwise
-	case 6:
-	  //jmp: 6 a
-	  //jump to <a>
 	case 7:
 	  //jt: 7 a b
 	  //if <a> is nonzero, jump to <b>
@@ -77,12 +107,6 @@ int runcmd(int x){
 	case 18:
 	  //ret: 18
 	  //remove the top element from the stack and jump to it; empty stack = halt
-	case 19:
-	  //out: 19 a
-	  //write the character represented by ascii code <a> to the terminal
-	  a = readnum();
-	  printf("%c",a);
-	  break;
 	case 20:
 	  //in: 20 a
 	  //read a character from the terminal and write its ascii code to <a>; it can be assumed that once input starts, it will continue until a newline is encountered; this means that you can safely read whole lines from the keyboard and trust that they will be fully read
@@ -92,10 +116,6 @@ int runcmd(int x){
 	  printf("Error - unimplemented code %d\n",x);
 	  return 2;
 
-	case 21:
-	  //noop
-	  printf("noop\n");
-	  break;
 	  
 
 	}
@@ -104,15 +124,27 @@ int runcmd(int x){
 	
 }
 
-void main(){
+
+
+int main(int c, char ** v){
 	printf("Starting ... \n");
+	printf("Reading into array\n");
 	fp=fopen("challenge.bin","rb");
-	int status=0;
 	int x=0;
-	while (status==0)
+	for (int i=0;i<MAX;i++)
 	{
 	  x=readnum();
-	  status=runcmd(x);
+	  if (x==-1){
+	    printf("Bad read of byte %d\n",i);
+	    return 1;
+	  }
+	  program[i]=x;
 	}
 	fclose(fp);
+	printf("Read complete\n");
+	idx=0;
+	while(runcmd()==0); // keep running till we exit
+	
+	
+	return 0;
 }
